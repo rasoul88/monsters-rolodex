f36abcac48ce894d651fd330cd7c57b7cb37f5ac
rasoul88 <rasoulsahraei75@gmail.com>
2019-12-13 08:46:15 +0330

    Updates

---
+++ precache-manifest.4ae9231f4c66594f358dd20004cf50ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c99621798d8679fb10bdda87f83ead57",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e98c5e9250f36b14b08b",
+    "url": "/monsters-rolodex/static/css/main.03dd96a7.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e98c5e9250f36b14b08b",
+    "url": "/monsters-rolodex/static/js/main.1ada28ae.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
